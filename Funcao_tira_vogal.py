70da894984e30b180aaa351be42a94da9d0a1b8f
MatheusVidigal <120541900+MatheusVidigalUFES@users.noreply.github.com>
2023-09-23 16:39:42 -0300

    Add files via upload

---
+++ Funcao_tira_vogal.py
@@ -0,0 +1,11 @@
+def tira_vogal(string):
+    lista_vogais=['a','e','i','o','u']
+    lista_consoante=[]
+    s = ''
+    for j in string:
+        if j not in lista_vogais:
+            lista_consoante.append(j)
+    for i in lista_consoante:
+        s= s+i
+    return s
+
